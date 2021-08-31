7cceebbd9494b0f748bebe5fdd63d9b1bcec9792
addr_18 <anggydahianaruiz99@gmail.com>
2021-08-31 14:07:58 -0500

    merge rama daniel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <div>
      <login /> 
-    <!--  <form @submit.prevent="login(user)">
-      <input type="email" placeholder="email" v-model="user.email" />
-      <input type="password" placeholder="password" v-model="user.password" />
-      <button type="submit">Acceder</button>
-    </form>
-    {{user}} -->
   </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
-import Login from '../components/Login.vue';
+import Login from '../components/Login/Login.vue';
 export default {
   data() {
     return {
